95f2d8c76ec194ab33e5fecb56fdb999d3961701
HB <sohaib.zeghouani@gmail.com>
2020-09-09 10:47:51 +0200

    first commit

---
+++ src/Entity/Raviole.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\RavioleRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass=RavioleRepository::class)
+ */
+class Raviole
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $titre;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $description;
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $quantite_dispo;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $prix;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPrix(): ?string
+    {
+        return $this->prix;
+    }
+
+    public function setPrix(?string $prix): self
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+
+    public function getQuantiteDispo(): ?int
+    {
+        return $this->quantite_dispo;
+    }
+
+    public function setQuantiteDispo(int $quantite_dispo): self
+    {
+        $this->quantite_dispo = $quantite_dispo;
+
+        return $this;
+    }
+}
